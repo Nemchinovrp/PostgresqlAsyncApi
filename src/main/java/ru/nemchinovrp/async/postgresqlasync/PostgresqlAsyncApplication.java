fa7e54de1e3136bcf882b7025e3a713aefeb3efd
nemchinovrp <nemchinovrp@yandex.ru>
2020-05-21 13:22:33 +0800

    start app

---
+++ src/main/java/ru/nemchinovrp/async/postgresqlasync/PostgresqlAsyncApplication.java
@@ -0,0 +1,13 @@
+package ru.nemchinovrp.async.postgresqlasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresqlAsyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nemchinovrp/async/postgresqlasync/PostgresqlAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nemchinovrp.async.postgresqlasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlAsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
